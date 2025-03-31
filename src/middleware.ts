58220e8bc227daec1ebb6363564df62ee29f6673
Stavros Liaskos <st.liaskos@gmail.com>
2025-03-31 23:06:09 +0200

    conditionally use auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,41 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { auth0 } from './lib/auth0';
 
 export async function middleware(req: NextRequest) {
   let accessToken = null;
   const authRes = await auth0.middleware(req);
 
-  try {
-    accessToken = await auth0.getAccessToken(req, authRes);
-    accessToken?.token && authRes.headers.set('Authorization', `Bearer ${accessToken.token}`);
-    // @ts-ignore
-    accessToken?.scope && authRes.headers.set('x-scope', `${accessToken.scope}`);
-  } catch (e) {
-    console.warn(e);
+  if (req.nextUrl.pathname.startsWith('/auth')) {
+    console.log(`\nAUTH Middleware called for url: ${req.url}\n`);
+    return authRes;
   }
 
-  console.log(`Middleware called for url: ${req.url}`);
-  console.warn(authRes.headers); // TODO debug only
-  return authRes;
+  const response = NextResponse.next();
+  console.log(`\nMiddleware called for url: ${req.url}\n`);
+
+  try {
+    accessToken = await auth0.getAccessToken(req, authRes);
+
+    accessToken?.token && response.headers.set('Authorization', `Bearer ${accessToken.token}`);
+    // @ts-ignore
+    accessToken?.scope && response.headers.set('x-scope', `${accessToken.scope}`);
+  } catch (e) {
+    // @ts-ignore
+    console.error(e?.message);
+  }
+  // console.warn(response.headers); // TODO debug only
+  return response;
 }
 
 export const config = {
   matcher: [
+    // '/auth/:path*',
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!_next/static|_next/image|sw.js|workbox|favicon.ico|sitemap.xml|robots.txt|manifest.json|noflash.js).*)',
+    '/((?!_next/static|_next/image|sw.js|workbox|app_icons|favicon.ico|sitemap.xml|robots.txt|manifest.json|noflash.js).*)',
   ],
 };
