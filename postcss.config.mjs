b65c4e953b84cdbb6e14599636177682cceba2c0
Stavros Liaskos <st.liaskos@gmail.com>
2025-06-23 11:45:42 +0200

    integrate depcheck

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,19 +3,3 @@ export default {
     '@tailwindcss/postcss': {},
   },
 };
-// module.exports = {
-//   plugins: [
-//     'tailwindcss',
-//     [
-//       '@fullhuman/postcss-purgecss',
-//       process.env.NODE_ENV === 'production'
-//         ? {
-//             // the paths to all template files
-//             content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-//             // function used to extract class names from the templates
-//             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-//           }
-//         : false,
-//     ],
-//   ],
-// };
