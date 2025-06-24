4c953dc93f16a66d21c7a444300bc79341259c32
Stavros Liaskos <st.liaskos@gmail.com>
2025-06-24 23:24:54 +0200

    rm unused lib

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -3,16 +3,11 @@ import { render, act } from '@testing-library/react';
 import ThemeProvider from '../contexts/Theme/ThemeProvider';
 import ArtistsListProvider from '../contexts/ArtistsList/ArtistsListProvider';
 import { setupServer } from 'msw/node';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <ArtistsListProvider>{children}</ArtistsListProvider>
-      </QueryClientProvider>
+      <ArtistsListProvider>{children}</ArtistsListProvider>
     </ThemeProvider>
   );
 };
