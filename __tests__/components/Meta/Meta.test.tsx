ed97dd20279ed01196c2ef0301a6a22cf9aaea79
Stalias <st.liaskos@gmail.com>
2025-06-30 22:31:15 +0200

    chore/meta: migrate meta (#100)

--- __tests__/components/Meta/Meta.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Meta from '@/components/Meta/Meta';
-import { render } from '@testing-library/react';
-
-describe('Meta', () => {
-  it('renders without data without crashing', () => {
-    render(<Meta />);
-  });
-});
