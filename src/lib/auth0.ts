36873fd93f54bf3fd5a395ef64898f77019447ad
Stavros Liaskos <st.liaskos@gmail.com>
2025-08-09 09:37:24 +0200

    fix/login: redirect to /profile

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,8 +1,11 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
+import { NavigationPaths } from '@/types/endpoints';
+
 export const auth0 = new Auth0Client({
   authorizationParameters: {
     scope: process.env.API_SCOPE,
     audience: process.env.API_AUDIENCE,
   },
+  signInReturnToPath: NavigationPaths.Profile,
 });
