1049f5399fdfade33444358031ce41805f54ff18
Stalias <st.liaskos@gmail.com>
2025-08-10 17:02:15 +0200

    feat/followers: add followers (#117)
    * feat/followers: add followers

--- src/components/ArtistsList/components/List/List.tsx
+++ src/components/ArtistsList/components/List/List.tsx
@@ -1,11 +1,14 @@
 import clsx from 'clsx/lite'; // Size (gzip): 140 bytes, CAUTION: Accepts ONLY string arguments!
 import React from 'react';
 
+import Badge from '@/components/Badge/Badge';
 import ButtonFollowArtist from '@/components/ButtonFollowArtist/ButtonFollowArtist';
 import LastFm from '@/components/Icons/lastfm';
 import Spotify from '@/components/Icons/spotify';
+import { listI18n } from '@/i18n';
 
 import { ArtistsListProp } from '../../ArtistsList';
+
 const ICON_SIZE = 30;
 
 const List = ({ artistsList, artistLoading, buttonAction }: ArtistsListProp) => {
@@ -15,33 +18,42 @@ const List = ({ artistsList, artistLoading, buttonAction }: ArtistsListProp) => 
 
   return (
     <>
-      {artistsList.map((artist, index: number) => (
-        <div
-          className="flex justify-between md:justify-center items-center dark:even:bg-gh-darkly even:bg-gray-100"
-          key={artist.id}
-        >
-          <p className="grow text-clip rr-text">{artist.name}</p>
-          <div className="flex gap-2 basis-2 mx-4 md:mx-8">
-            {artist.lastfmUri && (
-              <a className="inline" href={artist.lastfmUri} aria-label={artist.name}>
-                <LastFm width={ICON_SIZE} />
-              </a>
-            )}
-            {artist.spotifyUri && (
-              <a className="inline" href={artist.spotifyUri} aria-label={artist.name}>
-                <Spotify width={ICON_SIZE} />
-              </a>
-            )}
+      {artistsList.map((artist, index: number) => {
+        const followerCount = artist?.followerCount;
+        return (
+          <div
+            className="flex justify-between items-center dark:even:bg-gh-darkly even:bg-gray-100"
+            key={artist.id ?? index}
+          >
+            <p className="grow text-clip rr-text">{artist.name}</p>
+
+            <div className="flex items-center">
+              {!!followerCount && (
+                <Badge>{`${followerCount}${followerCount > 1 ? listI18n.followers : listI18n.follower}`}</Badge>
+              )}
+              <div className="flex gap-2 mx-4 md:mx-8 w-20">
+                {artist.lastfmUri && (
+                  <a className="inline" href={artist.lastfmUri} aria-label={artist.name}>
+                    <LastFm width={ICON_SIZE} />
+                  </a>
+                )}
+                {artist.spotifyUri && (
+                  <a className="inline" href={artist.spotifyUri} aria-label={artist.name}>
+                    <Spotify width={ICON_SIZE} />
+                  </a>
+                )}
+              </div>
+              <ButtonFollowArtist
+                artist={artist}
+                className={clsx('btn-small lg:ml-8 my-2', index % 2 && 'border-gh-dark!')}
+                disabled={!!artistLoading && artist.id === artistLoading}
+                loading={!!artistLoading && artist.id === artistLoading}
+                buttonAction={buttonAction}
+              />
+            </div>
           </div>
-          <ButtonFollowArtist
-            artist={artist}
-            className={clsx('btn-small lg:ml-8 my-2', index % 2 && 'border-gh-dark!')}
-            disabled={!!artistLoading && artist.id === artistLoading}
-            loading={!!artistLoading && artist.id === artistLoading}
-            buttonAction={buttonAction}
-          />
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
