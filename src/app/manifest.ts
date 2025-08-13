e4bc10c218ffd7305511b9d1f09cddb65bdb6281
Stavros Liaskos <st.liaskos@gmail.com>
2025-08-13 10:18:47 +0200

    fix/pwa: fix icons, links

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,29 +1,32 @@
 import type { MetadataRoute } from 'next';
 
 import { metaI18n } from '@/i18n';
+import { NavigationPaths } from '@/types/endpoints';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: metaI18n.title,
     short_name: metaI18n.short_name,
     description: metaI18n.description,
-    start_url: '/',
+    start_url: NavigationPaths.Profile,
     display: 'standalone',
-    prefer_related_applications: false,
     background_color: '#000000',
     theme_color: '#000000',
     icons: [
       {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
         src: '/app_icons/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
       },
       {
         src: '/app_icons/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable',
       },
     ],
   };
