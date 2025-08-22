aad6a5194ffd3bd0de7c26f0aae1b9c41b980953
Stavros Liaskos <st.liaskos@gmail.com>
2025-08-22 12:56:32 +0200

    fix/pointer: default

--- src/components/ArtistsList/components/List/List.tsx
+++ src/components/ArtistsList/components/List/List.tsx
@@ -30,7 +30,10 @@ const List = ({ artistsList, buttonAction }: ArtistsListProp) => {
             <div className="flex items-center">
               {!!followerCount && (
                 <Badge
-                  className={clsx(index % 2 ? 'dark:bg-gh-dark bg-white' : 'bg-gray-100 dark:bg-gh-darkly')}
+                  className={clsx(
+                    index % 2 ? 'dark:bg-gh-dark bg-white' : 'bg-gray-100 dark:bg-gh-darkly',
+                    'cursor-default',
+                  )}
                 >{`${followerCount}${followerCount > 1 ? listI18n.followers : listI18n.follower}`}</Badge>
               )}
               <div className="flex gap-2 mx-4 md:mx-8 w-20">
