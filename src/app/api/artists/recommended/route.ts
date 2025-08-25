81f92cde88cb31c60e43b00b75fba19d0007b8f2
Stavros Liaskos <st.liaskos@gmail.com>
2025-08-25 11:22:51 +0200

    fix: recommendations use middleware

--- src/app/api/artists/recommended/route.ts
+++ src/app/api/artists/recommended/route.ts
@@ -1,22 +1,24 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-import { auth0 } from '@/lib/auth0';
 import { API_Paths } from '@/types/endpoints';
 
-export async function getRecommendations() {
+export async function GET(req: NextRequest) {
   try {
-    const accessToken = await auth0.getAccessToken({ refresh: true });
+    const authorization = req.headers.get('Authorization');
 
     // const response = await fetch(`${process.env.API_BASE_URL}/${API_Paths.Recommended}?${req.nextUrl.searchParams}`, {
     const response = await fetch(`${process.env.API_BASE_URL}/${API_Paths.Recommended}?page=1&size=10`, {
       headers: {
-        authorization: `Bearer ${accessToken.token}`,
+        ...(authorization && { authorization }),
         'content-type': 'application/json',
       },
       method: 'GET',
     });
+    if (!response.ok) {
+      throw new Error(`Error fetching recommended artists: ${response.statusText}`);
+    }
     const data = await response.json();
-    return NextResponse.json(data);
+    return NextResponse.json(data.rows);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Internal Server Error', error }, { status: 500 });
--- src/types/endpoints.ts
+++ src/types/endpoints.ts
@@ -15,6 +15,7 @@ export const NavigationPaths: { [key: string]: string } = {
 export const Paths: { [key: string]: string } = {
   Auth: '/api/auth/me',
   Search: `api/artist/search`,
+  Recommended: `api/artists/recommended`,
   Follow: `api/me/follow`,
   FollowedArtists: `api/me/followed-artists`,
   UnfollowArtist: `api/me/unfollow`,
